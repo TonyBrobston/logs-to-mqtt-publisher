e8ca8ccb54fdd5a753dfc712f84ab3cfc439bd65
Tony Brobston <anthonybrobston@live.com>
2019-12-21 00:26:27 -0600

    fix: better import and only print changes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
-import * as chokidar from 'chokidar';
-const readLastLines = require('read-last-lines');
+import {watch} from 'chokidar';
+import {read} from 'read-last-lines';
 
 const filePath = process.env.FILE_PATH;
 
 if (filePath) {
-    chokidar.watch(filePath.toString()).on('all', (event, path) => {
-        readLastLines.read(path, 1).then((lines: string) => {
-            console.log(lines)
-        });
+    watch(filePath.toString()).on('all', (event, path) => {
+        if (event === 'change') {
+            read(path, 1).then((lines: string) => {
+                console.log(lines)
+            });
+        }
     });
 } else {
     const errorMessage = 'process.env.FILE_PATH is not valid.';
