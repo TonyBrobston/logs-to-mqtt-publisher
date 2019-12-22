17fe57a0e784f412865f690462961ab395a2742d
Tony Brobston <anthonybrobston@live.com>
2019-12-22 12:24:15 -0600

    test: poc done, adding tests

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
 import {watch} from 'chokidar';
 import {read} from 'read-last-lines';
+import {connect} from 'async-mqtt';
 
-export const start = (): void => {
+export const start = async (): Promise<void> => {
     const filePath = process.env.FILE_PATH;
+    const host = process.env.MQTT_HOST;
+    const port = process.env.MQTT_PORT ? process.env.MQTT_PORT : '1883';
+    const client = connect(`tcp://${host}:${port}`);
 
     if (filePath) {
-        watch(filePath.toString()).on('change', (path) => {
-            read(path, 1).then((lines: string) => {
-                console.log(lines)
-            });
+        watch(filePath.toString()).on('change', async (path) => {
+            const lines = await read(path, 1);
+            const {topic, message} = parse(lines);
+            await client.publish(topic, message);
         });
-    } else {
-        const errorMessage = 'process.env.FILE_PATH is not valid.';
-        console.log(errorMessage);
-        throw new Error(errorMessage);
-    }
+    }// else {
+    //    const errorMessage = 'process.env.FILE_PATH is not valid.';
+    //    console.log(errorMessage);
+    //    throw new Error(errorMessage);
+    //}
+}
+
+const parse = () => {
+    return {
+        topic: 'someTopic',
+        message: 'someMessage'
+    };
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,49 @@
-import {run} from '../src/index';
+import Chance from 'chance';
+import {watch} from 'chokidar';
+import {read} from 'read-last-lines';
+import {connect} from 'async-mqtt';
+import {start} from '../src/index';
+
+jest.mock('chokidar');
+jest.mock('read-last-times');
+jest.mock('mqtt');
+
+const chance = new Chance();
 
 describe('index', () => {
     it('should return hello world', async () => {
-        expect(run()).toBe('Hello World');
+        const filePath = chance.string();
+        process.env.FILE_PATH = filePath;
+        const mqttHost = chance.string();
+        process.env.MQTT_HOST = mqttHost;
+        const mqttPort = chance.string();
+        process.env.MQTT_PORT = mqttPort;
+        const publish = jest.fn();
+        const client = {
+            publish
+        }
+        connect.mockReturnValue(client);
+        const path = chance.string();
+        const on = jest.fn().mockImplementation((event, onCallback) => {
+            onCallback(path);
+        });
+        const watcher = {
+            on
+        };
+        watch.mockReturnValue(watcher);
+        const lines = chance.string();
+        read.mockResolvedValue(lines);
+
+        await start();
+
+        expect(watch)
+            .toHaveBeenCalledTimes(1)
+            .toHaveBeenCalledWith(filePath.toString());
+        expect(on)
+            .toHaveBeenCalledTimes(1)
+            .toHaveBeenCalledWith('chance', expect.any(Function));
+        expect(read)
+            .toHaveBeenCalledTimes(1)
+            .toHaveBeenCalledWith(path, 1);
     });
 });
