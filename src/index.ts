65bf08f972e81fcfcf2d1ba933ce1f0ba19f7beb
Tony Brobston <anthonybrobston@live.com>
2019-12-22 16:31:27 -0600

    refactor: getting closer to parsing a log line

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,37 @@ import {watch} from 'chokidar';
 import {read} from 'read-last-lines';
 
 export const start = async (): Promise<void> => {
-    const host = process.env.MQTT_HOST;
-    const port = process.env.MQTT_PORT;
-    const {publish} = await connectAsync(`tcp://${host}:${port}`);
-    const filePath = process.env.FILE_PATH;
-
-    if (filePath) {
-        await watch(filePath.toString()).on('change', async (path) => {
+    const logFilePath = process.env.LOG_FILE_PATH;
+    const logFileRegex = process.env.LOG_FILE_REGEX;
+    if (logFilePath && logFileRegex) {
+        const host = process.env.MQTT_HOST;
+        const port = process.env.MQTT_PORT;
+        const {publish} = await connectAsync(`tcp://${host}:${port}`);
+        const logFileRegexSplit = logFileRegex.split('/');
+        const regex = new RegExp(logFileRegexSplit[1], logFileRegexSplit[2])
+        await watch(logFilePath.toString()).on('change', async (path) => {
             const lines = await read(path, 1);
-            const {topic, message} = parse(lines);
-            await publish(topic, message);
+            const {topic, message} = parse(lines, regex);
+            if (topic && message) {
+                await publish(topic, message);
+            }
         });
     }
 }
 
-const parse = (lines: string) => {
-    return {
-        topic: 'someTopic',
-        message: 'someMessage'
-    };
+export const parse = (lines: string, regex: RegExp) => {
+    console.log('lines:', lines);
+    console.log('regex:', regex);
+    const found = lines.match(regex);
+    console.log(found);
+    if (found) {
+        const camera = found[0];
+        const event = found[1];
+        const message = found[2];
+        return {
+            topic: `${event}/${camera}`,
+            message
+        }
+    }
+    return {};
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,7 +2,7 @@ import {Chance} from 'chance';
 import {connectAsync} from 'async-mqtt';
 import {watch} from 'chokidar';
 import {read} from 'read-last-lines';
-import {start} from '../src/index';
+import {start, parse} from '../src/index';
 
 jest.mock('async-mqtt');
 jest.mock('chokidar');
@@ -13,12 +13,14 @@ const chance = new Chance();
 
 describe('index', () => {
     it('should start', async () => {
-        const filePath = chance.string();
-        process.env.FILE_PATH = filePath;
+        const logFilePath = chance.string();
+        process.env.LOG_FILE_PATH = logFilePath;
         const mqttHost = chance.string();
         process.env.MQTT_HOST = mqttHost;
         const mqttPort = chance.string();
         process.env.MQTT_PORT = mqttPort;
+        const logFileRegex = '/camera|event|message/g';
+        process.env.LOG_FILE_REGEX = logFileRegex;
         const publish = jest.fn();
         const client = {
             publish
@@ -32,18 +34,31 @@ describe('index', () => {
             on
         };
         (watch as jest.Mock).mockReturnValue(watcher);
-        const lines = chance.string();
+        const lines = 'camera,event,message';
         read.mockResolvedValue(lines);
 
         await start();
 
         expect(watch).toHaveBeenCalledTimes(1);
-        expect(watch).toHaveBeenCalledWith(filePath.toString());
+        expect(watch).toHaveBeenCalledWith(logFilePath.toString());
         expect(on).toHaveBeenCalledTimes(1);
         expect(on).toHaveBeenCalledWith('change', expect.any(Function));
         expect(read).toHaveBeenCalledTimes(1);
         expect(read).toHaveBeenCalledWith(path, 1);
         expect(publish).toHaveBeenCalledTimes(1);
-        expect(publish).toHaveBeenCalledWith('someTopic', 'someMessage');
+        expect(publish).toHaveBeenCalledWith('event/camera', 'message');
+    });
+
+    it('should parse', () => {
+        const event = 'Motion';
+        const camera = 'House Northeast';
+        const expectedMessage = 'start';
+        const logLine = `1577042817.781 2019-12-22 13:26:57.781/CST: INFO   [uv.analytics.motion] [AnalyticsService] [FCECDAD8B870|${camera}] ${event}Event type:${expectedMessage} event:28345 clock:10377014318 in AnalyticsEvtBus-11`;
+        const regex = /\[FCECDAD8B870\|House Northeast\]|MotionEvent|type:start|stop/g;
+
+        const {topic, message} = parse(logLine, regex);
+
+        expect(topic).toBe(`${event}/${camera}`);
+        expect(message).toBe(expectedMessage);
     });
 });
