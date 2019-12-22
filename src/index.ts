895c47b53aebaa0b81a1a8c1cf9b340a8a9a8def
Tony Brobston <anthonybrobston@live.com>
2019-12-22 11:04:36 -0600

    fix: making script available as a function

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import {watch} from 'chokidar';
 import {read} from 'read-last-lines';
 
-const filePath = process.env.FILE_PATH;
+export const start = (): void => {
+    const filePath = process.env.FILE_PATH;
 
-if (filePath) {
-    watch(filePath.toString()).on('change', (path) => {
-        read(path, 1).then((lines: string) => {
-            console.log(lines)
+    if (filePath) {
+        watch(filePath.toString()).on('change', (path) => {
+            read(path, 1).then((lines: string) => {
+                console.log(lines)
+            });
         });
-    });
-} else {
-    const errorMessage = 'process.env.FILE_PATH is not valid.';
-    console.log(errorMessage);
-    throw new Error(errorMessage);
+    } else {
+        const errorMessage = 'process.env.FILE_PATH is not valid.';
+        console.log(errorMessage);
+        throw new Error(errorMessage);
+    }
 }
-
