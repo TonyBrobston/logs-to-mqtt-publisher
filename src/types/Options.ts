db55b9e4130dc8c70e20c990db8563473d5aa948
Tony Brobston <anthonybrobston@live.com>
2019-12-26 19:54:25 -0600

    refactor: do not require any options

--- src/types/Options.ts
+++ src/types/Options.ts
@@ -1,17 +1,18 @@
 export interface Options extends SharedOptions {
+    logFilePath: string;
     logFileRegex: RegExp;
     mqttHost: string;
     mqttPort: string;
 }
 
 export interface InputOptions extends SharedOptions {
-    logFileRegex: string;
+    logFilePath?: string;
+    logFileRegex?: string;
     mqttHost?: string;
     mqttPort?: string;
 }
 
 export interface SharedOptions {
-    logFilePath: string;
     mqttUsername?: string;
     mqttPassword?: string;
 }
