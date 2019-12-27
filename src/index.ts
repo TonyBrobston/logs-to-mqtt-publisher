47531a763c054188144bf08ccae7063fce506ac0
Tony Brobston <anthonybrobston@live.com>
2019-12-26 18:23:26 -0600

    refactor: publish all message

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import {connectAsync} from 'async-mqtt';
 import {watch} from 'chokidar';
 import {read} from 'read-last-lines';
 
-import {InputOptions} from './types/Options';
 import {MqttPayload} from './types/MqttPayload';
+import {InputOptions} from './types/Options';
 import {Options} from './types/Options';
 
 import {parse} from './services/logService';
@@ -22,8 +22,6 @@ export const start = async (inputOptions: InputOptions): Promise<void> => {
         const line = await read(path, 1);
         const {topic, message}: MqttPayload = parse(line, logFileRegex);
 
-        if (topic && message) {
-            await publish(topic, message);
-        }
+        await publish(topic, message);
     });
 };
