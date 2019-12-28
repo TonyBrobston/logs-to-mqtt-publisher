f3451f515252608c02acd2dabc0bee8ea69c6fc8
Tony Brobston <anthonybrobston@live.com>
2019-12-27 21:34:37 -0600

    test: moving towards a test that doesn't use any mocks

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,70 +1,49 @@
 import {connectAsync} from 'async-mqtt';
-import {Chance} from 'chance';
-import {watch} from 'chokidar';
-import {read} from 'read-last-lines';
-import {start} from '../src/index';
-import {MqttPayload} from '../src/types/MqttPayload';
-
-jest.mock('async-mqtt');
-jest.mock('chokidar');
-jest.mock('read-last-lines');
-jest.mock('mqtt');
-
-const chance = new Chance();
+import {Server} from 'mosca';
 
 describe('index', () => {
-    const logFilePath = chance.string();
-    const logFileRegex = '/parentTopic|childTopic|message/g';
-    const mqttHost = chance.string();
-    const mqttPort = chance.string();
-    const inputOptions = {
-        logFilePath,
-        logFileRegex,
-        mqttHost,
-        mqttPort,
-    };
-    const publish = jest.fn();
-    const client = {
-        publish,
-    };
-    (connectAsync as jest.Mock).mockResolvedValue(client);
-    const path = chance.string();
-    const on = jest.fn().mockImplementation((payload: MqttPayload, onCallback: any) => {
-        onCallback(path);
-    });
-    const watcher = {
-        on,
-    };
-    (watch as jest.Mock).mockReturnValue(watcher);
-    const line = 'parentTopic,childTopic,message';
-    read.mockResolvedValue(line);
+    let server: any,
+        client: any;
 
     beforeAll(async () => {
-        await start(inputOptions);
+        server = await createServer({
+            host: 'localhost',
+            port: 1883,
+        });
+        client = await connectAsync('tcp://localhost:1883');
+        client.on('connect', () => {
+            console.log('client connected');
+        });
+        client.on('connectAsync', () => {
+            console.log('client connected async');
+        });
+        client.on('error', () => {
+            console.log('client errored');
+        });
+        client.on('message', (topic: any, message: any) => {
+            console.log('topic:', topic);
+            console.log('message:', message.toString());
+        });
     });
 
-    it('should call async-mqtt connectAsync', () => {
-        expect(connectAsync).toHaveBeenCalledTimes(1);
-        expect(connectAsync).toHaveBeenCalledWith(`tcp://${mqttHost}:${mqttPort}`);
+    afterAll(async () => {
+        await client.end();
+        server.close();
     });
 
-    it('should call chokidar watch', () => {
-        expect(watch).toHaveBeenCalledTimes(1);
-        expect(watch).toHaveBeenCalledWith(logFilePath.toString());
-    });
-
-    it('should call chokidar on change', () => {
-        expect(on).toHaveBeenCalledTimes(1);
-        expect(on).toHaveBeenCalledWith('change', expect.any(Function));
-    });
-
-    it('should call read-last-lines read', () => {
-        expect(read).toHaveBeenCalledTimes(1);
-        expect(read).toHaveBeenCalledWith(path, 1);
-    });
-
-    it('should call async-mqtt publish', () => {
-        expect(publish).toHaveBeenCalledTimes(1);
-        expect(publish).toHaveBeenCalledWith('parentTopic/childTopic', 'message');
+    it('should stand up a mqtt broker, subscriber, publish, and receive message', async () => {
+        console.log('in test');
+        const subscribed = await client.subscribe('presence');
+        await client.publish('presence', 'It works!');
+        console.log('subscribed', subscribed);
     });
 });
+
+const createServer = (settings: any): any => {
+    return new Promise((resolve: any): any => {
+        const server = new Server(settings);
+        server.on('ready', () => {
+            resolve(server);
+        });
+    });
+};
