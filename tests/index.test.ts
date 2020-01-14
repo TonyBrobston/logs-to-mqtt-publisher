17deb36f639f956a6188d27bf2a5cdcef5fc07af
Tony Brobston <anthonybrobston@live.com>
2020-01-14 15:10:39 -0600

    test: authenticate publisher client connection

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -6,8 +6,6 @@ import {Server} from 'mosca';
 import {start, stop} from '../src/index';
 
 const chance = new Chance();
-const expectedUsername = chance.string();
-const expectedPassword = chance.string();
 
 describe('index', () => {
     let server: Server,
@@ -18,13 +16,18 @@ describe('index', () => {
         min: 1000,
     });
     const filePath = `${__dirname}/test.log`;
+    const username = chance.string();
+    const password = chance.string();
 
     beforeEach(async () => {
         server = await createServerAsync({
-            host,
-            port,
-        });
-        client = await connectAsync(`tcp://${host}:${port}`);
+                host,
+                port,
+            },
+            username,
+            password,
+        );
+        client = await connectAsync(`tcp://${host}:${port}`, {username, password});
         closeSync(openSync(filePath, 'w'));
     });
 
@@ -56,9 +59,9 @@ describe('index', () => {
                 ],
                 mqtt: {
                     host,
-                    password: expectedPassword,
+                    password,
                     port,
-                    username: expectedUsername,
+                    username,
                 },
             });
 
@@ -71,50 +74,51 @@ describe('index', () => {
         });
     });
 
-//    describe('set environment variables', () => {
-//        it('should subscribe to a topic, watch a file, modify that file, and expect a message',
-//           async (done: () => void) => {
-//            const parentTopic = 'parentTopic2';
-//            const childTopic = 'childTopic2';
-//            const expectedTopic = `${parentTopic}/${childTopic}`;
-//            const expectedMessage = 'message2';
-//            const regularExpression = `/${parentTopic}|${childTopic}|${expectedMessage}/g`;
-//            await client.subscribe(expectedTopic);
-//
-//            const options = {
-//                logWatches: [
-//                    {
-//                        filePath,
-//                        regularExpressions: [
-//                            regularExpression,
-//                        ],
-//                    },
-//                ],
-//                mqtt: {
-//                    host,
-//                    port,
-//                },
-//            };
-//            process.env.OPTIONS = JSON.stringify(options);
-//
-//            await start();
-//
-//            writeFileSync(filePath, `${parentTopic},${childTopic},${expectedMessage}`);
-//            client.on('message', (topic: string, message: string) => {
-//                expect(topic).toBe(expectedTopic);
-//                expect(message.toString()).toBe(expectedMessage);
-//                done();
-//            });
-//        });
-//    });
+    describe('set environment variables', () => {
+        it('should subscribe to a topic, watch a file, modify that file, and expect a message',
+           async (done: () => void) => {
+            const parentTopic = 'parentTopic2';
+            const childTopic = 'childTopic2';
+            const expectedTopic = `${parentTopic}/${childTopic}`;
+            const expectedMessage = 'message2';
+            const regularExpression = `/${parentTopic}|${childTopic}|${expectedMessage}/g`;
+            await client.subscribe(expectedTopic);
+
+            const options = {
+                logWatches: [
+                    {
+                        filePath,
+                        regularExpressions: [
+                            regularExpression,
+                        ],
+                    },
+                ],
+                mqtt: {
+                    host,
+                    port,
+                },
+            };
+            process.env.OPTIONS = JSON.stringify(options);
+
+            await start();
+
+            writeFileSync(filePath, `${parentTopic},${childTopic},${expectedMessage}`);
+            client.on('message', (topic: string, message: string) => {
+                expect(topic).toBe(expectedTopic);
+                expect(message.toString()).toBe(expectedMessage);
+                done();
+            });
+        });
+    });
 });
 
-const createServerAsync = (settings: object): Promise<Server> => {
+const createServerAsync = (settings: {}, username: string, password: string): Promise<Server> => {
     return new Promise((resolve: (server: Server) => void): void  => {
         const server = new Server(settings);
         server.on('ready', () => {
-            server.authenticate = (client: {}, username: any, password: any, callback: any): void => {
-                callback(null, username === expectedUsername && password === expectedPassword);
+            server.authenticate = (client: {}, actualUsername: any, actualPassword: any, callback: any): void => {
+                const authenticated = actualUsername === username && actualPassword.toString() === password;
+                callback(null, authenticated);
             };
             resolve(server);
         });
